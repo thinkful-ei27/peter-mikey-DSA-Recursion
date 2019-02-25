21e07ca4a4591541dc836860a37a83119e7b12fa
Peter Ramos <peterdramos02@gmail.com>
2019-02-25 12:37:06 -0500

    complete reverse-str recursive dril

---
+++ reverse-str.js
@@ -0,0 +1,20 @@
+'use-strict';
+// Write a function that reverses a string. Take a string as input, 
+// reverse the string, and return the new string.
+
+// Input - string
+// Output - string-reversed
+
+// Sample I/O
+// reversString('hello') => olleh
+
+const reverseString = (str) => {
+  // base case 
+  if (str === '') return '';
+
+  return str[str.length - 1] + reverseString(str.slice(0, -1));
+};
+
+const result = reverseString('hello');
+
+console.log(result); // Output => olleh
