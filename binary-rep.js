4ac77a736983b79a43707700338e8ce398235744
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-25 16:18:08 -0500

    binary rep

--- binary-rep.js
+++ binary-rep.js
@@ -1,3 +1,4 @@
+'use strict';
 // Write out a recursice func that takes in a given number and prints out the 
 // binary representation of that number as the output.
 
@@ -6,5 +7,13 @@
 // binaryRep(10) => 1010
 
 const binaryRep = (num) => {
-  
+  //base case
+  if (num === 0) return '';
+  const binary = num % 2;
+  return binaryRep(Math.floor(num / 2)) + binary;
+
 };
+
+const answer = binaryRep(1346785445564);
+
+console.log(answer);
