c97a3ebaef408f8438ad0f3f3e0ba7e27d9ab85c
Peter Ramos <peterdramos02@gmail.com>
2019-02-25 16:27:51 -0500

    complete factorial drill

--- factorial.js
+++ factorial.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// Write a recursive func that find s the factorial of a given number
+
+// Sample I/O
+// facotrial(5) => 120 -- just 5 * 4 * 3 * 2 * 1 or 5 * !4 or num * facotrial(num-1)
+// factorial(3) => 6
+
+const factorial = (num) => {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+};
+
+const result = factorial(6);
+console.log(result);
